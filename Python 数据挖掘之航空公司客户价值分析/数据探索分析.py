358b0cf0bfffd770148eaff7cac8ea80d8e9ff9a
Chen Yu <58323844+18996677975@users.noreply.github.com>
2020-07-09 00:48:05 +0800

    Update 数据探索分析.py

--- Python 数据挖掘之航空公司客户价值分析/数据探索分析.py
+++ Python 数据挖掘之航空公司客户价值分析/数据探索分析.py
@@ -3,13 +3,14 @@
 
 import pandas as pd
 
-datafile= 'air_data.csv' #航空原始数据,第一行为属性标签
-resultfile = 'explore.xls' #数据探索结果表
+datafile = 'air_data.csv'  # 航空原始数据,第一行为属性标签
+resultfile = 'explore.xls'  # 数据探索结果表
 
-data = pd.read_csv(datafile, encoding = 'utf-8') #读取原始数据，指定UTF-8编码（需要用文本编辑器将数据装换为UTF-8编码）
+data = pd.read_csv(datafile, encoding='utf-8')  # 读取原始数据，指定UTF-8编码（需要用文本编辑器将数据装换为UTF-8编码）
 
-explore = data.describe(percentiles = [], include = 'all').T  # 包括对数据的基本描述，percentiles参数是指定计算多少的分位数表（如1/4分位数、中位数等）；T是转置，转置后更方便查阅
-explore['null'] = len(data)-explore['count']  # describe()函数自动计算非空值数，需要手动计算空值数
+explore = data.describe(percentiles=[],
+                        include='all').T  # 包括对数据的基本描述，percentiles参数是指定计算多少的分位数表（如1/4分位数、中位数等）；T是转置，转置后更方便查阅
+explore['null'] = len(data) - explore['count']  # describe()函数自动计算非空值数，需要手动计算空值数
 
 explore = explore[['null', 'max', 'min']]
 explore.columns = [u'空值数', u'最大值', u'最小值']  # 表头重命名
